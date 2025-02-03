8ac64df4b19b48f887dfd8637077df85c41a0ab2
Svpa04 <sofi04122001@gmail.com>
2025-02-03 18:01:05 -0400

    post detail done

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,8 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+#post detail view
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
