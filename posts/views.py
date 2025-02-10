7e58a3dcc115eccfb84421330ea137362d23c71d
Svpa04 <sofi04122001@gmail.com>
2025-02-10 17:19:24 -0400

    customs done

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -11,4 +13,6 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    
     
